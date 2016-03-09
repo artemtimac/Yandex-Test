95684b1e0a4e6350e3061b08844dddf9031aa95c
Artem Timac <artemtimac@mail.ru>
2016-03-09 12:11:16 +0300

    master 3.0

--- src/test/java/com/mycompany/app/YandexTest.java
+++ src/test/java/com/mycompany/app/YandexTest.java
@@ -42,7 +42,7 @@ public class YandexTest
 		$(byXpath("//*[contains(@class,'button')][span[text()='2']]")).shouldBe(visible).click();
         $(byText("Apple iPhone 5S 16Gb")).shouldBe(visible).click();
 
-        rating = Integer.parseInt($(byXpath("//*[@class='product-card__offer']//*[@data-rate]")).attr("data-rate")); // get something
+        rating = Integer.parseInt($(byXpath("//*[@class='product-card__offer']//*[@data-rate]")).attr("data-rate")); // get some
         System.out.printf("Оценка: %d" + '\n', rating);
     }
 }
