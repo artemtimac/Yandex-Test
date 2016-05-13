aa844a551a25075faeb37431a6fa1b85142e2094
Artem <artemtimac@mail.ru>
2016-05-13 19:30:56 +0300

    Update MarketPage.java

--- src/main/java/com/datateh/pages/MarketPage.java
+++ src/main/java/com/datateh/pages/MarketPage.java
@@ -15,7 +15,7 @@ public class MarketPage extends Page
         switch (section.toLowerCase())
         {
             case "phones" :
-                $(byText("Электроника")).shouldBe(visible).click();
+                $(byText("Электроника")).shouldBe(visible).hover();
                 $(byText("Мобильные телефоны")).shouldBe(visible).click();
                 phonesPage = page(PhonesPage.class);
                 break;
